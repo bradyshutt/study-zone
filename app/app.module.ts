70949e479ea16f89ae70fcaf790f48d75d70f452
Brady Shutt <bradyshutt@gmail.com>
2016-12-15 00:51:58 -0800

    added data bindings to login form

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { RegistrationComponent } from './registration.component';
+import { AccountService } from './account.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [
+     BrowserModule,
+     FormsModule,
+     HttpModule
+  ],
   declarations: [
      AppComponent,
      RegistrationComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+     AccountService
+  ],
+  bootstrap: [ AppComponent ]
 
 })
 export class AppModule { }
